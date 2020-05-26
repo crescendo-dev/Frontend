0722ad17f437ec3c9bde597c5a356b7a46a60215
kimbyeongsoo <kbsu147@naver.com>
2020-05-26 19:15:09 +0900

    개발하던거 복사

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      isModalOpen: false, 
+    }
+  }
+  
+  openModal = () => {
+    this.setState({ isModalOpen: true });
+  }
+  
+  closeModal = () => {
+    this.setState({ isModalOpen: false }); 
+  }
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="sidenav">
+        <div class="login-main-text">
+          <h2>Application<br></br> Login Page</h2>
+          <p>Login or register from here to access.</p>
+        </div>
+      </div>
+      <div class="main">
+        <div class="col-md-6 col-sm-12">
+          <div class="login-form">
+            <form>
+              <div class="form-group">
+                <label>Email</label>
+                <input type="text" class="form-control" placeholder="Email" />
+              </div>
+              <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" placeholder="Password" />
+              </div>
+              <button type="submit" class="btn btn-dark btn-space">Login</button>
+              <button type="submit" class="btn btn-secondary" onClick={this.openModal}>Register</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
