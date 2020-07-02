a4ebfaa3002628168deeaceafedcc6d0db557cba
kimbyeongsoo <kbsu147@naver.com>
2020-07-02 20:44:13 +0900

    회원가입 url 추가

--- src/RegisterPopup.js
+++ src/RegisterPopup.js
@@ -21,7 +21,7 @@ class RegisterPopup extends Component {
             alert(`패스워드를 확인하세요.`)
         } else {
 
-            axios.post('', {
+            axios.post('http://49.50.174.81:7777/server/SignUp', {
                 name: this.state.name,
                 email: this.state.email,
                 password: this.state.password
@@ -46,6 +46,7 @@ class RegisterPopup extends Component {
 
         const { name, email, password, passwordcheck } = this.state;
         const { appChange, appClick, appKeyPress } = this;
+
         return (
             <Popup trigger={<button>register</button>} position="right center">
                 <div>
